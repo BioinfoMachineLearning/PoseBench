9c5f74a639835f5d1bfca09ec375c2342460c420
Alex Morehead <acmwhb@missouri.edu>
2024-05-23 18:12:42 -0500

    Add doc config paths again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import datetime
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
