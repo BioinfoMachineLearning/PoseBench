1c4fce80be36f4914c8a816ace377d0e0c1d85bd
Alex Morehead <alex.morehead@gmail.com>
2024-12-18 10:33:18 -0600

    Add in Vina

--- notebooks/posebusters_method_interaction_analysis_plotting.py
+++ notebooks/posebusters_method_interaction_analysis_plotting.py
@@ -43,12 +43,12 @@ pd.options.mode.copy_on_write = True
 # %%
 # General variables
 baseline_methods = [
+    "vina_p2rank",
     "diffdock",
     "dynamicbind",
     "neuralplexer",
     # "rfaa",
     "chai-lab",
-    # "vina_p2rank",
 ]
 max_num_repeats_per_method = (
     1  # NOTE: Here, to simplify the analysis, we only consider the first run of each method
@@ -65,12 +65,12 @@ assert os.path.exists(
 
 # Mappings
 method_mapping = {
+    "vina_p2rank": "P2Rank-Vina",
     "diffdock": "DiffDock-L",
     "dynamicbind": "DynamicBind",
     "neuralplexer": "NeuralPLexer",
     "rfaa": "RoseTTAFold-AA",
     "chai-lab": "Chai-1",
-    "vina_p2rank": "P2Rank-Vina",
 }
 
 MAX_POSEBUSTERS_BENCHMARK_ANALYSIS_PROTEIN_SEQUENCE_LENGTH = 2000  # Only PoseBusters Benchmark targets with protein sequences below this threshold can be analyzed
