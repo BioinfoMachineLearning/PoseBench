6ae4c94aff82b40adaded2865bdcb5020334473a
Alex Morehead <acmwhb@missouri.edu>
2025-02-16 11:06:57 -0600

    Fix chai import

--- forks/chai-lab/chai_lab/data/__init__.py
+++ forks/chai-lab/chai_lab/data/__init__.py
@@ -1,3 +1,7 @@
 # Copyright (c) 2024 Chai Discovery, Inc.
 # Licensed under the Apache License, Version 2.0.
 # See the LICENSE file for details.
+
+from chai_lab.data import residue_constants
+
+__all__ = ["residue_constants"]
