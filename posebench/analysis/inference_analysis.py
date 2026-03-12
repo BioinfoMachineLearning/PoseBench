bddd10c0ef52bfaab01e0bab5c87c0b2e9a6a3ee
Alex Morehead <alex.morehead@gmail.com>
2026-03-12 11:55:59 -0700

    fix: Avoid potential side effects by breaking references to original molecule objects in select_primary_ligands_in_df

--- posebench/analysis/inference_analysis.py
+++ posebench/analysis/inference_analysis.py
@@ -170,7 +170,8 @@ def select_primary_ligands_in_df(
 
             if select_most_similar_pred_frag:
                 mol_pred_frags = [
-                    find_most_similar_frag(mol_true_frag, mol_pred_frags)[0]
+                    # Break reference to original molecule objects to avoid potential side effects
+                    find_most_similar_frag(Chem.Mol(mol_true_frag), [Chem.Mol(mol_pred_frag) for mol_pred_frag in mol_pred_frags])[0]
                     for mol_true_frag in mol_true_frags
                 ]
                 if not any(mol_pred_frags):
